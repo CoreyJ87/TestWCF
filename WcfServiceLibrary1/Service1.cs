0e6ec1ce5afd7cb1d5c4cdd7c03a2bc83bb226c0
synik4l <synik4l@gmail.com>
2013-03-08 15:58:25 -0600

    Init commit

---
+++ WcfServiceLibrary1/Service1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Web;
+
+namespace WcfJsonRestService
+{
+    public class Service1 : IService1
+    {
+        [WebInvoke(Method = "GET",
+                    ResponseFormat = WebMessageFormat.Json,
+                    UriTemplate = "{id}/{pname}")]
+        public Person GetData(string id,string pname)
+        {
+            // lookup person with the requested id 
+            return new Person()
+            {
+                Id = Convert.ToInt32(id),
+                Name = pname
+            };
+        }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
+
